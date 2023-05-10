d8b7444562546a326caa1a28716b7661e489a231
erik <ehansen1671@gmail.com>
2023-05-10 09:07:48 -0700

    turnin in

--- battleship.cc
+++ battleship.cc
@@ -1,38 +1,65 @@
 #include <iostream>
 #include <vector>
+#include <cctype>
 using namespace std;
+
+void printMap(vector<vector<char>> grid){
+	cout << "  A B C D E F G H I J" << endl;
+	int numCol = 0;
+	for(const auto &row : grid){
+		cout << numCol++ << " ";
+		for(const auto &col : row){
+			cout << col << " ";
+		}
+		cout << endl;
+	}
+}
+	
 
 int main(){
 	int winner = 1;
 	vector<vector<char>> grid(10, vector<char>(10, '#'));
 	system("clear");
+	/*
 	cout << "  A B C D E F G H I J" << endl;
-	int num = 0;
+	int numCol = 0;
 	for(const auto &row : grid){
-		cout << num++ << ' ';
+		cout << numCol++ << " ";
 		for(const auto &col : row){
-			cout << col << ' ';
+			cout << col << " ";
 		}
 		cout << endl;
 	}
+	*/
+	printMap(grid);
+	cout << endl;
+	vector<vector<char>> player1Personal(10, vector<char>(10, '#'));
+
+	/*
 	cout << "Player " << winner << " place your ships\n" << endl;
-	vector<vector<char>> shipBank;
-	int shipSize = 5;
-	for(int i = 0; i < 10; i++){
-		for(int j = 0; j < shipSize; j++){
-			shipBank.at(i).at(j) = 'O';
-			for(int k = 5; k > shipSize; k--){
-				shipSize--;
-				shipBank.at(i).at(j) = '#';
-			}
-		}
-	}
-	for(auto &row : shipBank){
-		for(auto &col : row){
-			cout << col << ' ';
+	vector<vector<char>> shipBank = {
+        {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', ' '},
+        {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', ' '},
+        {'O', 'O', 'O', 'O', 'O', 'O', ' ', ' ', ' ', ' '},
+        {'O', 'O', 'O', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {'O', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
+    };
+	for(const auto &row : shipBank){
+		for(const auto &col : row){
+			cout  << col << "   ";
 		}
 		cout << endl;
 	}
+	cout << endl;
+
+	cout << "Enter a letter and number (like A5)" << endl;
+	char letter;
+	int num;
+	cin >> letter;
+	cin >> num;
+	letter = toupper(letter);
+	int letterCord = letter - letter;
+	*/
 
 
 }
