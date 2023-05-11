8fcd15dbb539c1ac0caa7e63fa8c57bc37d927a2
erik <ehansen1671@gmail.com>
2023-05-11 01:10:27 -0700

    workin on battleship

--- battleship.cc
+++ battleship.cc
@@ -3,6 +3,7 @@
 #include <cctype>
 using namespace std;
 
+
 void printMap(vector<vector<char>> grid){
 	cout << "  A B C D E F G H I J" << endl;
 	int numCol = 0;
@@ -14,7 +15,46 @@ void printMap(vector<vector<char>> grid){
 		cout << endl;
 	}
 }
+void printShips(vector<vector<char>> shipBank){
+	for(const auto &row : shipBank){
+		for(const auto &col : row){
+        	cout << col << "   ";
+        }
+        cout << endl;
+	}
+}
+void addShip(vector<vector<char>> &playerMap, vector<vector<char>> &shipBank){
+	cout << "Place your ship!" << endl;
+	cout << "Select a ship to add! (Enter C, B, D, S, or P)" << endl;
+	char shipChoice;
+	cin >> shipChoice;
+	shipChoice = toupper(shipChoice);
+	for(size_t i = 0; i < shipBank.at(0).size(); i++){
+		if(shipBank.at(i).at(0) == shipChoice){
+			for(int j = 0; j < 5; j++){ 
+				shipBank.at(i).erase(shipBank.at(i).begin() + j);
+			}
+		continue;
+		}
+	}
+	printShips(shipBank);
+	return;
+	cout << endl << endl << endl;
+	cout << shipBank.at(0).at(0) << " [0][0]" << endl;
+	cout << shipBank.at(0).at(1) << " [0][1]" << endl;
+	cout << shipBank.at(1).at(0) << " [1][0]" << endl;
+
+	cout << "Enter a letter and number (like A5)" << endl;
+	char letter;
+	int num;
+	cin >> letter;
+	cin >> num;
+	letter = toupper(letter);
+	int letterCord = letter - letter;
+	printShips(shipBank);
 	
+
+}
 
 int main(){
 	int winner = 1;
@@ -33,33 +73,36 @@ int main(){
 	*/
 	printMap(grid);
 	cout << endl;
-	vector<vector<char>> player1Personal(10, vector<char>(10, '#'));
-
-	/*
-	cout << "Player " << winner << " place your ships\n" << endl;
-	vector<vector<char>> shipBank = {
-        {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', ' '},
-        {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', ' '},
-        {'O', 'O', 'O', 'O', 'O', 'O', ' ', ' ', ' ', ' '},
-        {'O', 'O', 'O', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {'O', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
-    };
-	for(const auto &row : shipBank){
-		for(const auto &col : row){
-			cout  << col << "   ";
+		vector<vector<char>> player1Map(10, vector<char>(10, '#'));
+		cout << "Your battleships: " << endl << endl;
+		printMap(player1Map);
+		
+		cout << "Player " << winner << " place your ships\n" << endl;
+		vector<vector<char>> shipBank = {
+        	{'C', 'B', 'B', 'S', 'S', 'S', 'P', 'P', 'P', 'P'},
+        	{'C', 'B', 'B', 'S', 'S', 'S', 'P', 'P', 'P', 'P'},
+        	{'C', 'B', 'B', 'S', 'S', 'S', ' ', ' ', ' ', ' '},
+        	{'C', 'B', 'B', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        	{'C', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
+    	};
+		for(const auto &row : shipBank){
+			for(const auto &col : row){
+				cout << col << "   ";
+			}
+			cout << endl;
 		}
+		addShip(player1Map, shipBank);
+		/*
 		cout << endl;
-	}
-	cout << endl;
-
-	cout << "Enter a letter and number (like A5)" << endl;
-	char letter;
-	int num;
-	cin >> letter;
-	cin >> num;
-	letter = toupper(letter);
-	int letterCord = letter - letter;
-	*/
+		cout << "Place your ship!" << endl;	
+		cout << "Enter a letter and number (like A5)" << endl;
+		char letter;
+		int num;
+		cin >> letter;
+		cin >> num;
+		letter = toupper(letter);
+		int letterCord = letter - letter;
+*/
 
 
 }
